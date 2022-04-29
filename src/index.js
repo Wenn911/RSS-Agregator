91e1e7e406ab41bbac784e0e681d10d530edf599
Rostislav Kuznetsov <wenn123@yandex.ru>
2022-04-29 22:40:50 +0300

    first step

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+console.log('helloworld!');
---
+++ webpack.config.cjs
@@ -0,0 +1,32 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  devServer: {
+    historyApiFallback: true,
+    static: {
+      directory: path.join(path.resolve(), 'dist'),
+    },
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+      // favicon: './assets/favicon.png',
+    }),
+  ],
+};
