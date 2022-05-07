fae2e3537164b7c331ebb62abf324aeb27c6eac9
Rostislav Kuznetsov <wenn123@yandex.ru>
2022-05-07 19:54:41 +0300

    Сократил код, изменил логику вотчера

--- src/init.js
+++ src/init.js
@@ -1,66 +1,37 @@
-import onChange from 'on-change';
 import i18next from 'i18next';
-import controller from './updateRSS.js';
-import ru from './locales/ru.js';
+import onChange from 'on-change';
+import render from './renders.js';
+import resources from './locales/resources.js';
+import view from './view.js';
+// Переписал логику вотчера, убрал отдельный стейт, сократил код
+export default () => {
+  const elements = {
+    form: document.getElementById('form'),
+    input: document.getElementById('input'),
+    addBtn: document.getElementById('addBtn'),
+    feedback: document.getElementById('feedback'),
+    feedNode: document.getElementById('feeds'),
+    postsNode: document.getElementById('posts'),
+  };
 
-import {
-  formRender, postsRender, UIRender, renderFeeds,
-} from './view.js';
-
-const runApp = () => {
-  const defaultLanguage = 'ru';
   const state = {
-    lng: defaultLanguage,
-    rssForm: {
-      state: null,
-      inputStatus: 'unblocked',
-      errors: {},
-    },
+    lng: 'ru',
     feeds: [],
     posts: [],
+    urls: [],
+    error: null,
+    readonly: false,
+    openPost: null,
   };
 
-  const uiState = {};
-
-  const i18nInstance = i18next.createInstance();
-  i18nInstance.init({
+  i18next.init({
     lng: state.lng,
-    debug: false,
-    resources: {
-      ru,
-    },
+    resources,
   });
 
-  const selectors = {
-    formElement: document.querySelector('.rss-form'),
-    inputElement: document.querySelector('.form-control'),
-    submitButton: document.querySelector('button[type="submit"]'),
-    notificationElement: document.querySelector('.feedback'),
-    feedContainer: document.querySelector('.feeds'),
-    postsContainer: document.querySelector('.posts'),
-  };
-
-  const watchedUIState = onChange(uiState, () => {
-    UIRender(uiState);
+  const watchedState = onChange(state, render(elements, i18next), {
+    ignoreKeys: ['urls'],
   });
 
-  const watchedState = onChange(state, (path) => {
-    switch (path) {
-      case 'rssForm.state':
-        formRender(state, selectors, i18nInstance);
-        break;
-      case 'feeds':
-        renderFeeds(state, selectors, i18nInstance);
-        break;
-      case 'posts':
-        postsRender(state, selectors, watchedUIState, i18nInstance);
-        UIRender(uiState);
-        break;
-      default:
-    }
-  });
-
-  controller(state, watchedState, selectors);
+  view(watchedState, elements, i18next);
 };
-
-export default runApp;
