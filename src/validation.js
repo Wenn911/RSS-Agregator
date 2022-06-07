be17e7195b2a7628aad0e9486b218959dd1c8884
Rostislav Kuznetsov <wenn123@yandex.ru>
2022-06-07 14:54:37 +0300

    delete unnecessary files

--- src/validation.js
+++
@@ -1,8 +0,0 @@
-import { string } from 'yup';
-
-export default ({ urls }) => {
-  const schema = string('errors.empty')
-    .url('errors.url')
-    .notOneOf(urls, 'errors.uniq');
-  return schema;
-};
